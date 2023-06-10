eaac86af5c1ff6940657876ba1dccc2c3303a01c
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-10 12:58:18 +0530

    abc

--- 2_useReducer/src/components/MainHeader/Navigation.js
+++ 2_useReducer/src/components/MainHeader/Navigation.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React , {useContext} from 'react';
 import AuthContext from '../../Store/auth-context';
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
+  const ctx = useContext(AuthContext);
   return (
-    <AuthContext.Consumer>
-      { (ctx)=> {
-         return (
-              <nav className={classes.nav}>
+    
+    <nav className={classes.nav}>
       <ul>
         {ctx.isLoggedIn && (
           <li>
@@ -26,10 +25,6 @@ const Navigation = (props) => {
         )}
       </ul>
     </nav>
-         )
-      }}
-    
-    </AuthContext.Consumer>
   );
 };
 
